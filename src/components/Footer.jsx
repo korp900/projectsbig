5758ef4f27e61f2303802c09727f67d3a1ab6b5c
korp900 <korp900@mail.ru>
2021-04-12 22:56:53 +0300

    необходимо отладка стилей (теряет путь) отладка Footer и прочих страниц: Страницы накладываются друг на друга. Изменить текст ссылки. Отрисовать админку Отрисовать Личный кабинет с функцыаналом

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,66 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export class Footer extends React.Component {
     render() {
-        return (
-            <footer>
+        return (<div className="footer">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-8 col-md-10 mx-auto">
-                            <ul className="list-inline text-center">
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <span className="fa-stack fa-lg">
-                                          <i className="fas fa-circle fa-stack-2x"></i>
-                                          <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <span className="fa-stack fa-lg">
-                                          <i className="fas fa-circle fa-stack-2x"></i>
-                                          <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="#">
-                                        <span className="fa-stack fa-lg">
-                                          <i className="fas fa-circle fa-stack-2x"></i>
-                                          <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                                        </span>
-                                    </a>
-                                </li>
+                        <div className="col-lg-3">
+                            <p className="mb-4"><img src="images/logo_footer.png" alt="Image" className="img-fluid"/>
+                            </p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor,
+                                iusto iure.</p>
+                            <p><Link to="#">Learn More</Link></p>
+                        </div>
+                        <div className="col-lg-3">
+                            <h3 className="footer-heading"><span>Solutions</span></h3>
+                            <ul className="list-unstyled">
+                                <li><Link to="#">Investment Bonds</Link></li>
+                                <li><Link to="#">Financial Funds</Link></li>
+                                <li><Link to="#">Growth Business</Link></li>
+                                <li><Link to="#">Lifetime Support</Link></li>
+                                <li><Link to="#">Advanced Accounting</Link></li>
                             </ul>
-                            <p className="copyright text-muted">Copyright &copy; EDU0201 KORP900 TIPTOP05 2020</p>
+                        </div>
+                        <div className="col-lg-3">
+                            <h3 className="footer-heading"><span>Services</span></h3>
+                            <ul className="list-unstyled">
+                                <li><Link to="#">Investment Bonds</Link></li>
+                                <li><Link to="#">Financial Funds</Link></li>
+                                <li><Link to="#">Growth Business</Link></li>
+                                <li><Link to="#">Lifetime Support</Link></li>
+                                <li><Link to="#">Advanced Accounting</Link></li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-3">
+                            <h3 className="footer-heading"><span>Contact</span></h3>
+                            <ul className="list-unstyled">
+                                <li><Link to="#">Help Center</Link></li>
+                                <li><Link to="#">Support Community</Link></li>
+                                <li><Link to="#">Press</Link></li>
+                                <li><Link to="#">Share Your Story</Link></li>
+                                <li><Link to="#">Our Supporters</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="copyright">
+                                <p>
+                                    Copyright &copy;
+                                    <script>document.write(new Date().getFullYear());</script>
+                                    All rights reserved | This template is made with <i className="icon-heart"
+                                                                                        aria-hidden="true"></i> by <Link
+                                    to="https://colorlib.com" target="_blank">Colorlib</Link>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </footer>
+            </div>
+
         )
     }
 }
