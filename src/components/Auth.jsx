629c0bd3c49ef74553ec9358d4d0a09e177b5ef6
korp900 <korp900@mail.ru>
2021-04-14 06:45:34 +0300

    auth

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React from 'react';
 
 export class Auth extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            pass: ""
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("email", this.state.email);
+        formData.append("pass", this.state.pass);
+        fetch("http://f92553mg.beget.tech/php/handlerAuth.php", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(result => console.log(result));
     }
 
     render() {
         return (
-            <h1> About</h1>
+            <div className="container">
+                <div className="col-sm-5 mx-auto">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="mb-3">
+                            <input value={this.state.email} onChange={this.handleInput} name="email" type="text"
+                                   className="form-control" placeholder="login"/>
+                        </div>
+                        <div className="mb-3">
+                            <input value={this.state.pass} onChange={this.handleInput} name="pass" type="password"
+                                   className="form-control" placeholder="pass"/>
+                        </div>
+                        <div className="mb-3 text-center">
+                            <input type="submit" value="Войти" className="btn btn-primary"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
